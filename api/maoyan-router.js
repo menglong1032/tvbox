4da12d088f850caffcbebc738e73a001c77e4ba6
龙霞 <49477249+menglong1032@users.noreply.github.com>
2026-09-17 02:20:43 +0800

    remove temporary Maoyan router

--- api/maoyan-router.js
+++
@@ -1,9 +0,0 @@
-const maoyan = require('./maoyan');
-const trailers = require('./trailers');
-
-module.exports = async function handler(req, res) {
-  if (String(req.query.action || '') === 'videos') {
-    return trailers(req, res);
-  }
-  return maoyan(req, res);
-};
